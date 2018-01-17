f5fa2076dbe075cae41b2915cde87a18a9f42fdf
Sanjeev Saroy <saroy94@gmail.com>
2018-01-17 17:57:21 +0000

    organised files into folders based on filetype

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Firebase Config
+var config = {
+  apiKey: "AIzaSyAM8AE6Mjan44_M1aLQt0g2UAIBBjDLJFY",
+  authDomain: "project-594279054854224271.firebaseapp.com",
+  databaseURL: "https://babypitstop.firebaseio.com",
+  projectId: "project-594279054854224271",
+  storageBucket: "gs://project-594279054854224271.appspot.com",
+  messagingSenderId: "334368862486",
+};
+
+firebase.initializeApp(config);
