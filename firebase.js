96d1ab7aba16d1dca71906693dd98263cf8c5f81
Sanjeev Saroy <saroy94@gmail.com>
2018-01-17 17:55:57 +0000

    all facilities being pulled on a loaded map

--- firebase.js
+++ firebase.js
@@ -2,7 +2,8 @@
 var config = {
   apiKey: "AIzaSyAM8AE6Mjan44_M1aLQt0g2UAIBBjDLJFY",
   authDomain: "project-594279054854224271.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  databaseURL: "https://babypitstop.firebaseio.com",
+  projectId: "project-594279054854224271",
   storageBucket: "gs://project-594279054854224271.appspot.com",
   messagingSenderId: "334368862486",
 };
--- home.js
+++ home.js
@@ -24,3 +24,66 @@ logoutBtn.click( function() {
     console.log(error);
   });
 });
+
+// Load maps
+var map;
+
+function initMap() {
+
+  var uluru = {lat: -25.363, lng: 131.044};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+}
+
+// Load facilities
+var database = firebase.database();
+var facilitiesRef = database.ref('facilities');
+var locationsRef = database.ref('locations')
+var geofire = new GeoFire(locationsRef);
+var openWindow;
+
+facilitiesRef.on('value', function(snapshot) {
+  snapshot.forEach(function (childsnapshot) {
+    var key = childsnapshot.key;
+
+    // Get facility location
+    geofire.get(key).then(function(location) {
+
+      // Place marker
+      var childData = childsnapshot.val();
+
+      var contentString =
+      '<h1>' + childData.name + '</h1>'
+      + "Changing : " + childData.facilities.changing + '</br>'
+      + "Feeding: " + childData.facilities.feeding + '</br>'
+      + "Warming : " + childData.facilities.warming + '</br>';
+
+      // Create infoWindow
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      // Create a marker
+      var pos = {lat: location[0], lng: location[1]};
+      var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+      });
+
+      // Add a marker listener
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+
+        // Close existing info windows that are open
+        if (openWindow != null) {
+          openWindow.close();
+        }
+
+        openWindow = infowindow;
+      });
+    });
+  });
+});
